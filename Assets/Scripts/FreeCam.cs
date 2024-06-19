60f1ca992d6ff1aaf7c7b1a2d4383b34892d4b52
NAND_ONOSAKA <32193548+NANDOHAD@users.noreply.github.com>
2024-06-19 09:53:28 +0900

    カメラの操作系を修正

--- Assets/Scripts/FreeCam.cs
+++ Assets/Scripts/FreeCam.cs
@@ -13,7 +13,9 @@ public class FreeCam : MonoBehaviour
     public InputField panSpeed;
     public InputField movSpeed;
     public float zoomSpeed = 10.0f;
-    // Start is called before the first frame update
+    public Vector3 minPosition = new Vector3(-5f, 0f, -10f);
+    public Vector3 maxPosition = new Vector3(5f, 10f, 10f);
+
     void Start()
     {
         mousePosition = Input.mousePosition;
@@ -21,41 +23,46 @@ public class FreeCam : MonoBehaviour
         movSpeed.text = (tSpeed * 100).ToString();
     }
 
-    // Update is called once per frame
-void FixedUpdate()
-{
-    Vector3 diff = mousePosition - Input.mousePosition;
-    if (canMove)
+    void FixedUpdate()
     {
-        if (Input.GetMouseButton(1) && !es.IsPointerOverGameObject())
+        Vector3 diff = mousePosition - Input.mousePosition;
+        if (canMove)
         {
-            transform.Rotate(new Vector3(diff.y * rSpeed, -diff.x * rSpeed, 0), Space.Self);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
-        }
+            if (Input.GetMouseButton(1) && !es.IsPointerOverGameObject())
+            {
+                transform.Rotate(new Vector3(diff.y * rSpeed, -diff.x * rSpeed, 0), Space.Self);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+            }
 
-        // マウスの中ボタンでカメラの上下左右移動
-        if (Input.GetMouseButton(2))
-        {
-            transform.Translate(new Vector3(diff.x * tSpeed, diff.y * tSpeed, 0), Space.Self);
-        }
+            if (Input.GetMouseButton(2))
+            {
+                Vector3 proposedTranslation = new Vector3(diff.x * tSpeed, diff.y * tSpeed, 0);
+                Vector3 newPosition = transform.position + transform.TransformDirection(proposedTranslation);
+                newPosition.x = Mathf.Clamp(newPosition.x, minPosition.x, maxPosition.x);
+                newPosition.y = Mathf.Clamp(newPosition.y, minPosition.y, maxPosition.y);
+                newPosition.z = Mathf.Clamp(newPosition.z, minPosition.z, maxPosition.z);
+                transform.position = newPosition;
+            }
 
-        // マウスホイールによるズームインとズームアウト
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        if (scroll != 0.0f)
-        {
-            transform.Translate(Vector3.forward * scroll * zoomSpeed, Space.Self);
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            if (scroll != 0.0f)
+            {
+                Vector3 proposedZoom = Vector3.forward * -scroll * zoomSpeed;
+                Vector3 newPosition = transform.position + transform.TransformDirection(proposedZoom);
+                newPosition.z = Mathf.Clamp(newPosition.z, minPosition.z, maxPosition.z);
+                transform.position = newPosition;
+            }
         }
+        mousePosition = Input.mousePosition;
     }
-    mousePosition = Input.mousePosition;
-}
 
     public void updateSpeeds()
     {
         float speed;
-        if (float.TryParse(panSpeed.text,out speed))
+        if (float.TryParse(panSpeed.text, out speed))
             rSpeed = speed / 100;
 
-        if (float.TryParse(movSpeed.text,out speed))
+        if (float.TryParse(movSpeed.text, out speed))
             tSpeed = speed / 100;
     }
 }
